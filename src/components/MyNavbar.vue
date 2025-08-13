f186af77f930a9e704479ab5fdf2d5c1021d38e3
JustFr4nck <francyperry07@gmail.com>
2025-08-13 11:56:07 +0200

    My services alpha update + Various changes

--- src/components/MyNavbar.vue
+++ src/components/MyNavbar.vue
@@ -5,62 +5,56 @@
                 <div class="absolute inset-y-0 pt-7 pl-30 linkSpace">
                     <h2 class="titleDiv">JustFranck</h2>
                 </div>
-                
-                <div class="absolute inset-y-0 right-0 pt-9 pr-100 linkSpace">
 
-                    <a class="text-lg m-6 group relative w-max">
-                        <span>
-                            Home
-                        </span>
-                        <span
-                            class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-                        <span
-                            class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-                    </a>
+                <div class="absolute inset-y-0 right-0 pt-9 pr-70 linkSpace">
 
-                    <a @click="scrollToAbout" class="text-lg m-6 group relative w-max">
-                        <span>
-                            About
-                        </span>
-                        <span
-                            class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-                        <span
-                            class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-                    </a>
+                    <div class="ml-25">
 
-                    <a @click="scrollToServices"  class="text-lg m-6 group relative w-max">
-                        <span>
-                            Services
-                        </span>
-                        <span
-                            class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-                        <span
-                            class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-                    </a>
+                        <a @click="scrollToAbout" class="text-lg m-6 group relative w-max">
+                            <span>
+                                About
+                            </span>
+                            <span
+                                class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
+                            <span
+                                class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
+                        </a>
 
-                    <a class="text-lg m-6 group relative w-max">
-                        <span>
-                            Skills
-                        </span>
-                        <span
-                            class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-                        <span
-                            class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-                    </a>
-                    
-                    <a class="text-lg m-6 group relative w-max">
-                        <span>
-                            Contact
-                        </span>
-                        <span
-                            class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-                        <span
-                            class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-                    </a>
+                        <a @click="scrollToServices" class="text-lg m-6 group relative w-max">
+                            <span>
+                                Services
+                            </span>
+                            <span
+                                class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
+                            <span
+                                class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
+                        </a>
+
+                        <a class="text-lg m-6 group relative w-max">
+                            <span>
+                                Skills
+                            </span>
+                            <span
+                                class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
+                            <span
+                                class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
+                        </a>
+
+                        <a class="text-lg m-6 group relative w-max">
+                            <span>
+                                Contact
+                            </span>
+                            <span
+                                class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
+                            <span
+                                class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
+                        </a>
+
+                    </div>
 
                     <div class="vertical-divider"></div>
                     <div class="ml-150 -mt-15">
-                        <button @click="visitCv" class="btnCv" >Download CV 📄</button>
+                        <button @click="visitCv" class="btnCv">Download CV 📄</button>
                     </div>
                 </div>
             </div>
@@ -72,26 +66,26 @@
 
 <script setup>
 
-            //cv button  
-       
-            function visitCv(){
+//cv button  
 
-                window.open("https://europa.eu/europass/eportfolio/api/eprofile/shared-profile/francesco-perrotta/3bc81d7c-3ed2-4015-aa6f-07ea6c0f8319?view=html","_blank");
+function visitCv() {
 
-            }
+    window.open("https://europa.eu/europass/eportfolio/api/eprofile/shared-profile/francesco-perrotta/3bc81d7c-3ed2-4015-aa6f-07ea6c0f8319?view=html", "_blank");
 
-            //scroll to about
-            function scrollToAbout(){
-                const aboutId = document.getElementById("aboutId");
-                aboutId.scrollIntoView({behavior: 'smooth'});
-            }
-            
-            //scroll to services
-            function scrollToServices(){
-                const servicesId = document.getElementById("servicesId");
-                servicesId.scrollIntoView({behavior: 'smooth'});
-            }
-            
-            
-   
+}
+
+//scroll to about
+function scrollToAbout() {
+    const aboutId = document.getElementById("aboutId");
+    aboutId.scrollIntoView({ behavior: 'smooth' });
+}
+
+//scroll to services
+function scrollToServices() {
+    const servicesId = document.getElementById("servicesId");
+    servicesId.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+
 </script>
--- src/components/MyServices.vue
+++ src/components/MyServices.vue
@@ -1,43 +1,47 @@
 <template>
-    <div class="flex items-center justify-center pt-50 pb-50">
+    <div class="flex items-center justify-center pt-50 pb-30">
         <h1 class="cool-font text-4xl font-extrabold text-center">My Services</h1>
     </div>
     <div class="pb-100">
         <div class="flex flex-row">
-            <div class="basis-1/2 ml-10 mr-5">
-                <div class="card bg-base-300 shadow-sm cardStyle">
-                    <div class="card-body">
-                        <h2 class="card-title cool-font-reverse">Large Card</h2>
-                        <p class="cool-font">A card component has a figure, a body part, and inside body there are title
+            <div class="basis-1/2 ml-10 mr-5 text-center">
+                <div class="card shadow-sm cardStyle">
+                    <div class="card-body flex items-center">
+                        <img class="w-10" src="../assets/images/icons/frontEndIcon.png" alt="">
+                        <h2 class="text-2xl font-semibold">Front-end developer</h2>
+                        <p class="">A card component has a figure, a body part, and inside body there are title
                             and actions parts</p>
                     </div>
                 </div>
             </div>
-            <div class="basis-1/2 ml-5 mr-10">
-                <div class="card bg-base-300 shadow-sm cardStyle">
-                    <div class="card-body">
-                        <h2 class="card-title cool-font-reverse">Large Card</h2>
-                        <p class="cool-font">A card component has a figure, a body part, and inside body there are title
+            <div class="basis-1/2 ml-5 mr-10 text-center">
+                <div class="card shadow-sm cardStyle">
+                    <div class="card-body flex items-center">
+                        <img class="w-10" src="../assets/images/icons/backEndIcon.png" alt="">
+                        <h2 class="text-2xl font-semibold">Back-end developer</h2>
+                        <p class="">A card component has a figure, a body part, and inside body there are title
                             and actions parts</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="flex flex-row mt-10">
+        <div class="flex flex-row mt-10 text-center">
             <div class="basis-1/2 ml-10 mr-5">
-                <div class="card bg-base-300  shadow-sm cardStyle">
-                    <div class="card-body">
-                        <h2 class="card-title cool-font-reverse">Large Card</h2>
-                        <p class="cool-font">A card component has a figure, a body part, and inside body there are title
+                <div class="card shadow-sm cardStyle">
+                    <div class="card-body flex items-center">
+                        <img class="w-10" src="../assets/images/icons/webDesignerIcon.png" alt="">
+                        <h2 class="text-2xl font-semibold">Web Designer</h2>
+                        <p class="">A card component has a figure, a body part, and inside body there are title
                             and actions parts</p>
                     </div>
                 </div>
             </div>
-            <div class="basis-1/2 ml-5 mr-10">
-                <div class="card bg-base-300 shadow-sm cardStyle">
-                    <div class="card-body">
-                        <h2 class="card-title cool-font-reverse">Large Card</h2>
-                        <p class="cool-font">A card component has a figure, a body part, and inside body there are title
+            <div class="basis-1/2 ml-5 mr-10 text-center">
+                <div class="card shadow-sm cardStyle">
+                    <div class="card-body flex items-center">
+                        <img class="w-10" src="../assets/images/icons/systemBuilderIcon.png" alt="">
+                        <h2 class="text-2xl font-semibold">System builder</h2>
+                        <p class="">A card component has a figure, a body part, and inside body there are title
                             and actions parts</p>
                     </div>
                 </div>
