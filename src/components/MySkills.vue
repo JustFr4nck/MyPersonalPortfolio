1d5a84bf55ab539621579ff0bb2d1bb10e2f7627
JustFr4nck <francyperry07@gmail.com>
2025-08-17 19:10:14 +0200

    MySkills inizialized

--- src/components/MySkills.vue
+++ src/components/MySkills.vue
@@ -1,3 +1,19 @@
 <template>
-    <p>hello world</p>
+    <div class="pt-50 flex items-center justify-center pb-30">
+        <h1 class="cool-font text-4xl font-extrabold text-center">MySkills</h1>
+    </div>
+    <div class="flex flex-row justify-center">
+
+        <div class="basis-1/3 text-center">
+            <h4 class="cool-font text-xl font-bold">Frontend</h4>
+        </div>
+        <div class="basis-1/3 text-center">
+            <h4 class="cool-font text-xl font-bold">Backend</h4>
+        </div>
+        <div class="basis-1/3 text-center">
+            <h4 class="cool-font text-xl font-bold">Tools</h4>
+        </div>
+
+    </div>
+    
 </template>
