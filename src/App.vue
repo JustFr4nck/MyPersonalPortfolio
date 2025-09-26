be4573559192709d39b24b10f1322794fdeb018c
JustFr4nck <francyperry07@gmail.com>
2025-09-26 12:28:21 +0200

    setup commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <RouterLink to="/"/>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
