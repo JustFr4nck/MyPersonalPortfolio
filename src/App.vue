a8efa7da4054c1f2914a1bd52795c862f6d180a5
JustFr4nck <francyperry07@gmail.com>
2025-09-27 09:26:53 +0200

    footer added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import HomeView from './views/HomeView.vue';
+import MyFooter from './components/MyFooter.vue';
 </script>
 
 <template>
   <body class="bg-gray-900 h-screen">
     <HomeView />
+    <MyFooter/>
   </body>
 
 </template>
--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,3 +1,29 @@
 <template>
-    <p></p>
+    <footer id="contactId" class=" md:p-2 flex flex-col items-center footer footer-horizontal footer-center bg-white text-base-content rounded lg:p-10 text-lg">
+        <nav>
+            <div class="flex flex-row justify-center gap-5">
+                <a href="https://discord.gg/4muQxBsM" target="_blank">
+                    <Icon :icon="'streamline-logos:discord-logo-2-solid'" class="text-gray-900" width="40" height="40" viewBox="0 0 24 24"></Icon>
+                </a> 
+                <a target="_blank" href="https://github.com/JustFr4nck">
+                    <Icon :icon="'streamline-logos:github-logo-2-solid'" class="text-gray-900" width="40" height="40" viewBox="0 0 24 24"></Icon>
+                </a> 
+                <a target="_blank" href="https://www.linkedin.com/in/francesco-perrotta-228b28374/">
+                    <Icon :icon="'streamline-logos:linkedin-logo-block'" class="text-gray-900" width="40" height="40" viewBox="0 0 24 24"></Icon>
+                </a>
+                <a target="_blank" href="https://t.me/Just_Fr4nck">
+                    <Icon :icon="'streamline-logos:telegram-logo-2-solid'" class="text-gray-900" width="40" height="40" viewBox="0 0 24 24"></Icon>
+                </a>
+            </div>
+        </nav>
+        <aside class="flex flex-row text-center pt-5">
+            <p>Copyright © JustFranck&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; perrotta.francesco.job@gmail.com</p>
+        </aside>
+    </footer>
 </template>
+
+<script setup lang="ts">
+
+    import {Icon} from '@iconify/vue';
+
+</script>
