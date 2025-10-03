ecb5ec26cff7cdf6746bff8856b9c262387231f0
JustFr4nck <francyperry07@gmail.com>
2025-10-03 18:54:35 +0200

    carousel text added

--- src/views/MyProjects.vue
+++ src/views/MyProjects.vue
@@ -6,23 +6,28 @@
             </div>
         </div>
         <div class="carousel w-130 lg:w-200 rounded-xl">
-            <div id="slide1" class="carousel-item relative w-full">
-                <img src="../assets/images/commitSite.png" alt="">
-                <h1 class="text-white">Ciaoooooooooooooooooooooooooooooooooooooo</h1>
+            <div id="slide1" class="carousel-item relative w-full flex flex-col items-center">
+                <img class="w-100 lg:w-170 rounded-xl" src="../assets/images/commitSite.png" alt="">
+                <h1 class="text-white font-bold text-xl">Commit booking</h1>
+                <h4 class="text-white">A meeting room booking site for the company commit software</h4>
                 <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide3" class="btn btn-circle">❮</a>
                     <a href="#slide2" class="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide2" class="carousel-item relative w-full">
-                <img src="../assets/images/dragonTris.png" alt="">
+            <div id="slide2" class="carousel-item relative w-full flex flex-col items-center">
+                <img class="w-100 lg:w-170 rounded-xl" src="../assets/images/dragonTris.png" alt="">
+                <h1 class="text-white font-bold text-xl">Dragon tris</h1>
+                <h4 class="text-white">A meeting room booking site for the company commit software</h4>
                 <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide1" class="btn btn-circle">❮</a>
                     <a href="#slide3" class="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide3" class="carousel-item relative w-full">
-                <img src="../assets/images/piniaTasks.png" alt="">
+            <div id="slide3" class="carousel-item relative w-full flex flex-col items-center">
+                <img class="w-100 lg:w-170 rounded-xl" src="../assets/images/piniaTasks.png" alt="">
+                <h1 class="text-white font-bold text-xl">Pinia tasks</h1>
+                <h4 class="text-white">A meeting room booking site for the company commit software</h4>
                 <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide2" class="btn btn-circle">❮</a>
                     <a href="#slide1" class="btn btn-circle">❯</a>
